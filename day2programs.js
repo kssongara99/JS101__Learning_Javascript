91d722958ba389ab6ef04954daf51c6036b5d56c
kuldeep singh <kssongara99@gmail.com>
2023-09-06 10:54:10 +0530

    day 2 program problem

---
+++ day2programs.js
@@ -0,0 +1,35 @@
+Mathematical operator
+1. Addition (+)
+2. subtraction (-)
+3.Multiplication(*)
+4.Division(/)
+
+
+return a boolean value
+1. greater than (>)
+2. greater than or equal to (>=)
+3 . less than (<)
+4. less than or equal to (<=)
+5.Equality (==)
+6. Inequality(!=)
+7.Strict Equality( ===)
+8.Strict Inequality(!==)
+
+
+problem-->
+let a=9;
+let b=7;
+
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a==b);
+console.log(a!=b);
+console.log(a===b);
+console.log(a!==b);
